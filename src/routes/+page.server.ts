0c37c90b44b033886e6fe36e08e0afd4ab880a38
Jordan B <jsbursik@gmail.com>
2025-08-01 15:46:55 -0500

    implemented zod for validation on the backend

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,28 +1,41 @@
 import type { Actions } from "@sveltejs/kit";
 import { fail } from "@sveltejs/kit";
+
+import { z } from "zod";
 import emailValidator from "node-email-verifier";
 
-interface FormData {
-  first_name?: string;
-  last_name?: string;
-  email?: string;
-  phone?: string;
-  emailError?: string;
-  fake?: boolean;
-}
+const CouponFormSchema = z.object({
+  first_name: z
+    .string()
+    .min(3, "First Name is required.")
+    .regex(/[a-zA-Z]+/, "First name must be letters"),
+  last_name: z
+    .string()
+    .min(3, "Last Name is required.")
+    .regex(/[a-zA-Z]+/, "Last name must be letters"),
+  email: z
+    .string()
+    .email("Invalid Email Address.")
+    .refine(async (email) => {
+      return await emailValidator(email);
+    }, "Email is either fake or temporary"),
+  phone: z.string().min(10, "Phone is required"),
+});
 
 export const actions: Actions = {
   default: async ({ request }) => {
-    const data = await request.formData();
-    const formObject: Record<string, string> = {};
-    for (const [key, value] of data.entries()) {
-      formObject[key] = value as string;
+    const form = await request.formData();
+    const data = Object.fromEntries(form.entries());
+
+    const parsed = await CouponFormSchema.safeParseAsync(data);
+
+    if (!parsed.success) {
+      const { fieldErrors } = parsed.error.flatten();
+      return fail(400, { errors: fieldErrors, values: data });
     }
-    if (formObject.email) {
-      const isValid = await emailValidator(formObject.email);
-      if (!isValid) {
-        return fail(400, { ...formObject, emailError: "Invalid Email Address", fake: true } satisfies FormData);
-      }
-    }
+
+    const { first_name, last_name, email, phone } = parsed.data;
+
+    console.log(`${first_name} ${last_name} ${email} ${phone}`);
   },
 };
