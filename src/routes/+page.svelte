3688ac49a03477c278e5e1ebcc39627accd53963
Jordan B <jsbursik@gmail.com>
2025-08-01 15:47:36 -0500

    reworked with zod errors

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,20 +3,17 @@
   import InputField from "$lib/form-components/input-field.svelte";
 
   let { form }: { form: ActionData } = $props();
-
-  let first_name = $state(form?.first_name || "");
-  let last_name = $state(form?.last_name || "");
-  let email = $state(form?.email || "");
-  let phone = $state(form?.phone || "");
+  let fieldErrors = form?.errors;
+  let values = form?.values;
 </script>
 
 <div class="container fit">
   <form method="POST" class="column-form">
     <h2 class="form-header">Coupon Form:</h2>
-    <InputField id="first_name" label="First Name" placeholder="John" required bind:value={first_name} />
-    <InputField id="last_name" label="Last Name" placeholder="Smith" required bind:value={last_name} />
-    <InputField class={{ invalid: form?.fake }} type="email" id="email" label="Email" required bind:value={email} />
-    <InputField type="tel" id="phone" label="Phone" required bind:value={phone} />
-    <button class="btn btn-primary" style="width: 100%;">Get Coupon Code!</button>
+    <InputField id="first_name" label="First Name" placeholder="John" required value={values?.first_name || ""} error={fieldErrors?.first_name?.[0]} />
+    <InputField id="last_name" label="Last Name" placeholder="Smith" required value={values?.last_name || ""} error={fieldErrors?.last_name?.[0]} />
+    <InputField type="email" id="email" label="Email" required value={values?.email || ""} error={fieldErrors?.email?.[0]} />
+    <InputField type="tel" id="phone" label="Phone" required value={values?.phone || ""} error={fieldErrors?.phone?.[0]} />
+    <button class="btn btn-primary" style="margin-top: 2rem">Get Coupon Code!</button>
   </form>
 </div>
