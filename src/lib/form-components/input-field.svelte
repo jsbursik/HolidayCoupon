77691902b4ecc03256c6adf9ac57c90899790aaa
Jordan B <jsbursik@gmail.com>
2025-08-01 15:47:16 -0500

    added error handling

--- src/lib/form-components/input-field.svelte
+++ src/lib/form-components/input-field.svelte
@@ -2,10 +2,11 @@
   import { inputValidation } from "$lib/actions/inputValidation";
   import type { InputType } from "$appTypes";
 
-  let { type = "text" as InputType, id, label, value = $bindable(), ...props } = $props();
+  let { type = "text" as InputType, id, label, value = $bindable(), error = "", ...props } = $props();
 </script>
 
 <span class="form-control">
   <label for={id}>{label}</label>
-  <input {type} {id} name={id} bind:value use:inputValidation={{ type }} {...props} />
+  <input class={{ invalid: error }} {type} {id} name={id} bind:value use:inputValidation={{ type }} {...props} />
+  {#if error}<small class="error">{error}</small>{/if}
 </span>
