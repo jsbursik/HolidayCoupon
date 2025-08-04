29a4d08a1aa3351419faf72332047e151d1cccb8
Jordan B <jsbursik@gmail.com>
2025-08-04 16:19:25 -0500

    coupon form result page, needs some spice

---
+++ src/routes/coupon/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { page } from "$app/state";
+  let code = page.url.searchParams.get("code");
+</script>
+
+{#if code}
+  <div class="container fit">
+    <h1>{code}</h1>
+  </div>
+{:else}
+  <h1>No Coupon Code Found</h1>
+{/if}
