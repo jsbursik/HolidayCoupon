56a9304142fbf1f3b0bbfecff0c07268fe635ce7
Jordan B <jsbursik@gmail.com>
2025-08-06 14:35:02 -0500

    made coupon page better-looking

--- src/routes/coupon/+page.svelte
+++ src/routes/coupon/+page.svelte
@@ -5,7 +5,9 @@
 
 {#if code}
   <div class="container fit">
-    <h1>{code}</h1>
+    <h2>Your Coupon Code:</h2>
+    <h1 style="color: var(--accent-clr); margin-top: 1.5rem;">{code}</h1>
+    <p style="max-width: 15rem; text-align: center; font-weight: bold;">Make sure to screenshot this to redeem it!</p>
   </div>
 {:else}
   <h1>No Coupon Code Found</h1>
