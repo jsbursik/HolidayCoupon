82957219d4a11954521c0790df7723ba90c2761e
Jordan Bursik <jsbursik@gmail.com>
2025-08-05 20:30:33 -0500

    cleaned up form formatting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import type { ActionData } from "./$types";
-  import InputField from "$lib/form-components/input-field.svelte";
+  import Input from "$lib/form-components/input.svelte";
 
   let { form }: { form: ActionData } = $props();
   let fieldErrors = form?.errors;
@@ -8,12 +8,14 @@
 </script>
 
 <div class="container fit">
-  <form method="POST" class="column-form">
-    <h2 class="form-header">Coupon Form:</h2>
-    <InputField id="first_name" label="First Name" placeholder="John" required value={values?.first_name || ""} error={fieldErrors?.first_name?.[0]} />
-    <InputField id="last_name" label="Last Name" placeholder="Smith" required value={values?.last_name || ""} error={fieldErrors?.last_name?.[0]} />
-    <InputField type="email" id="email" label="Email" required value={values?.email || ""} error={fieldErrors?.email?.[0]} />
-    <InputField type="tel" id="phone" label="Phone" required value={values?.phone || ""} error={fieldErrors?.phone?.[0]} />
-    <button class="btn btn-primary" style="margin-top: 2rem">Get Coupon Code!</button>
+  <form method="POST">
+    <div class="column">
+      <h2>Coupon Form:</h2>
+      <Input id="first_name" label="First Name" placeholder="John" required value={values?.first_name || ""} error={fieldErrors?.first_name?.[0]} />
+      <Input id="last_name" label="Last Name" placeholder="Smith" required value={values?.last_name || ""} error={fieldErrors?.last_name?.[0]} />
+      <Input type="email" id="email" label="Email" required value={values?.email || ""} error={fieldErrors?.email?.[0]} />
+      <Input type="tel" id="phone" label="Phone" required value={values?.phone || ""} error={fieldErrors?.phone?.[0]} />
+      <button class="btn btn-primary">Get Coupon Code!</button>
+    </div>
   </form>
 </div>
